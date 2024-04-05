92e75a3ea1a71d5e4bcba43c5bcfb97e541cd7f2
Vinay Jatav <vinay.jatav.dev@gmail.com>
2024-04-05 22:52:00 +0530

    nodemailer setup done

---
+++ src/app/api/mailer/route.ts
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import { env } from "../../../env";
+
+const tranporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secure: false,
+    auth:{
+        user: env.GMAIL_FROM,
+        pass: env.GMAIL_APP_PASSWORD
+    }
+})
+
+export async function POST(request: Request){
+    const message = await request.json();
+    try{
+        tranporter.sendMail(message, (err, info)=>{
+            console.log(info.envelope);
+            console.log(info.messageId)
+        });
+        return NextResponse.json({ status: 200 });
+    } catch(error){
+        return NextResponse.json({ status: 500 });
+    }
+}
